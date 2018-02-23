7d8609d65f6069ac05dcf25adec3c90f27b392d8
Jacob Reed <jreed91@gmail.com>
2018-02-23 10:52:09 -0600

    Fix environment issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD4ZB2mOS_kUljHypuxULNbSYxGmXj1_d4',
+    authDomain: 'vehiclemaintenance-7f9dc.firebaseapp.com',
+    databaseURL: 'https://vehiclemaintenance-7f9dc.firebaseio.com',
+    projectId: 'vehiclemaintenance-7f9dc',
+    storageBucket: 'vehiclemaintenance-7f9dc.appspot.com',
+    messagingSenderId: '20763243692'
+  }
 };
